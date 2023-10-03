f7c142a82cf9552986b6cfc183b54b2d810309ab
Freja Fischer <frej657e@stud.kea.dk>
2023-10-03 09:04:28 +0200

    finish

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
 gsap.registerPlugin(ScrollTrigger);
+
+const sections = document.querySelectorAll("section");
+
+sections.forEach((section) => {
+  const h2 = section.querySelector("h2");
+  const ps = section.querySelectorAll("p");
+
+  gsap
+    .timeline({
+      scrollTrigger: {
+        trigger: section,
+        markers: true,
+        start: "top 50%",
+        end: "+=200px",
+        scrub: true,
+      },
+    })
+    .from(h2, {
+      opacity: 0,
+    })
+    .from(ps, {
+      opacity: 0,
+      y: 100,
+      stagger: 0.5,
+    });
+});
